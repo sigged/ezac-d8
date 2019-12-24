7ef24b169cfe5551005d643c29dcae6c9b134633
Evert Fekkes <evert@efekkes.nl>
2019-12-24 20:51:21 +0100

    ezacLeden removed array type from validate and submit

--- modules/ezacKisten/src/Form/EzacKistenUpdateForm.php
+++ modules/ezacKisten/src/Form/EzacKistenUpdateForm.php
@@ -190,7 +190,7 @@ class EzacKistenUpdateForm extends FormBase
     /**
      * {@inheritdoc}
      */
-    public function validateForm(array &$form, FormStateInterface $form_state)
+    public function validateForm( &$form, FormStateInterface $form_state) //removed array type
     {
 
         // perform validate for edit of record
@@ -206,7 +206,7 @@ class EzacKistenUpdateForm extends FormBase
      * {@inheritdoc}
      * @throws \Exception
      */
-    public function submitForm(array &$form, FormStateInterface $form_state)
+    public function submitForm( &$form, FormStateInterface $form_state) // removed array type
     {
         $messenger = \Drupal::messenger();
 
