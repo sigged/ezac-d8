7dcc68be28177d193d8f56dcd607d04573af1027
Evert Fekkes <evert@efekkes.nl>
2020-12-20 18:24:11 +0100

    Ezac modules drupal 9

---
+++ modules/contrib/Ezac/src/Model/EzacStorage.php
@@ -0,0 +1,340 @@
+<?php
+
+namespace Drupal\ezac\Model;
+
+use Drupal;
+use Drupal\Core\Database\Database;
+use Exception;
+use PDO;
+
+// use Drupal\Core\Database\Connection;
+
+// use Drupal\Core\Database\Query\PagerSelectExtender;
+
+/**
+ * Class EzacStorage.
+ *
+ * Generic database commands for Ezac tables
+ */
+
+/**
+ * Provides the interface for the EzacStorage class
+ */
+class EzacStorage
+{
+
+    // define id property
+    public $id = 0;
+
+    //$config = \Drupal::config('Ezac.database');
+    //Database::setActiveConnection($config->get('name');
+
+    protected const dbName = 'Ezac';
+
+    /**
+     *
+     * @param $table
+     * @param array $condition
+     * @return mixed
+     */
+    public static function ezacCount($table, $condition = array())
+    {
+
+        // use ezacIndex
+        return count(self::ezacIndex($table, $condition));
+
+        /*
+        // Read all fields from a Ezac table.
+        // EZAC database is outside the Drupal structure
+        Database::setActiveConnection(self::dbName);
+        $db = Database::getConnection();
+
+        $select = $db->select($table); // geen alias gebruikt
+        $select->fields($table);
+
+        // Add each field and value as a condition to this query.
+        foreach ($condition as $field => $test) {
+            if (is_array($test)) {
+                $value = $test["value"];
+                $operator = $test["operator"];
+                $select->condition($field, $value, $operator);
+            } else $select->condition($field, $test);
+        }
+        // Return the result in object format.
+        $record_count = $select->countQuery()->execute()->fetchField();
+
+        // return to standard Drupal database
+        Database::setActiveConnection();
+
+        return $record_count;
+        */
+    } // ezacCount
+
+    /**
+     * Read index from the database using a filter array.
+     *
+     * @param string $table
+     *   The EZAC table to be used for the database operation
+     * @param array $condition
+     *   The condition for the update as array (field => value) (=)
+     *   alternative: field[value => value, operator => operator]
+     * @param string $field
+     * @param string $sortkey
+     *  the field for which the read should be sorted
+     * @param $sortdir string default 'ASC' sort direction
+     * @param int $from for range selection default NULL
+     * @param int $range default NULL the range for the query
+     * @param bool $unique default FALSE, return DISTINCT results
+     * @return array
+     *   An array of objects containing the loaded entries if found.
+     */
+    public static function ezacIndex($table, $condition = NULL, $field = 'id', $sortkey = NULL, $sortdir = 'ASC', $from = NULL, $range = NULL, $unique = FALSE)
+    {
+
+        // Read unique index from a Ezac table.
+        // EZAC database is outside the Drupal structure
+        Database::setActiveConnection(self::dbName);
+        $db = Database::getConnection();
+
+        $select = $db->select($table, 't');
+        $select->addField('t', $field);
+
+        // Add each field and value as a condition to this query.
+
+        /*
+         * a condition entry is either a field and value
+         * OR a field pointing to an array of value and operator
+         * OR an array pointing to an orConditionGroup, by key "OR"
+         */
+
+        foreach ((array)$condition as $field => $test) {
+            // condition can be a simple field => value pair for EQUALS (default test)
+            //   or contain value and operator keys for other tests
+            if (is_array($test)) {
+              // combined condition with value(s) and operator
+              if ($field == 'OR') {
+                // test is part of an orGroup
+                $orGroup = $select->orConditionGroup();
+                foreach ($test as $field2 => $test2) {
+                  if (is_array($test2)) {
+                    // combined condition
+                    $value = $test2["value"];
+                    $operator = $test2["operator"];
+                    $orGroup->condition($field2, $value, $operator);
+                  }
+                  else {
+                    //single condition
+                    $orGroup->condition($field2, $test2, '=');
+                  }
+                } // orGroup element
+                $select->condition($orGroup);
+              } //orGroup
+              else {
+                // combined condition
+                $value = $test["value"];
+                $operator = $test["operator"];
+                $select->condition($field, $value, $operator);
+              }
+              // simple condition
+            } else $select->condition($field, $test);
+        }
+        // sort the result
+        if (isset($sortkey)) {
+            $select->orderBy($sortkey, $sortdir);
+        }
+        // add range for pager
+        if (isset($range)) {
+            $select->range($from, $range);
+        }
+        // Return the result in array format.
+        if ($unique) $index = $select->distinct()->execute()->fetchCol();
+        else $index = $select->execute()->fetchCol();
+
+        // return to standard Drupal database
+        Database::setActiveConnection();
+
+        return (array)$index;
+
+    } // ezacIndex
+
+    /**
+     * Insert a record in the EZAC database
+     *
+     * @param string $table
+     *   The EZAC table to be used for the database operation
+     * @return int
+     *   The id of the inserted record
+     *
+     */
+    public function ezacCreate($table)
+    {
+
+        // EZAC database is outside the Drupal structure
+        // select EZAC database outside Drupal structure
+        Database::setActiveConnection(self::dbName);
+        $db = Database::getConnection();
+
+        // create record
+        $return_value = NULL;
+        // build array from object fields
+        $entry = get_object_vars($this);
+        try {
+            $return_value = $db->insert($table)
+                ->fields($entry)
+                ->execute();
+        } catch (Exception $e) {
+            $messenger = Drupal::messenger();
+            $message = "db_insert failed. Message = " . $e->getMessage();
+            $messenger->addMessage($message, $messenger::TYPE_ERROR);
+        }
+        // set id value
+        $entry['id'] = $return_value;
+
+
+        // return to standard Drupal database
+        Database::setActiveConnection();
+
+        return $return_value;
+
+    } // ezacCreate
+
+    /**
+     * Read from the database
+     * The id of the record to be read is taken from the object->id
+     * @param string $table
+     *   The EZAC table to be used for the database operation
+     * @param string $className default stdClass
+     * @return object className
+     *   An object containing the loaded entry if found.
+     */
+    protected function ezacRead($table, $className = "stdClass")
+    {
+
+        // define prefix for EZAC tables
+        // $table = 'EZAC_' .$table;
+
+        // Read all fields from a Ezac table.
+        // select EZAC database outside Drupal structure
+        Database::setActiveConnection(self::dbName);
+        $db = Database::getConnection();
+
+        $select = $db->select($table); // geen alias gebruikt
+        $select->fields($table); // all fields of the table
+        $select->condition('id', $this->id); // select this record
+
+        // Return the result as an object
+        $select->execute()->setFetchMode(PDO::FETCH_CLASS, $className); //prepare class
+        $record = $select->execute()->fetchObject();
+
+        // return to standard Drupal database
+        Database::setActiveConnection();
+
+        return $record;
+
+    } //ezacRead
+
+  /**
+   * @param $table
+   * @param $condition
+   * @param string $className
+   */
+  static public function ezacReadAll($table, $condition, $className = "stdClass")
+    {
+      // Read all fields from a Ezac table.
+      // select EZAC database outside Drupal structure
+      Database::setActiveConnection(self::dbName);
+      $db = Database::getConnection();
+
+      $select = $db->select($table); // geen alias gebruikt
+      $select->fields($table); // all fields of the table
+
+      // Add each field and value as a condition to this query.
+      foreach ((array)$condition as $field => $test) {
+        // condition can be a simple field => value pair for EQUALS (default test)
+        //   or contain value and operator keys for other tests
+        if (is_array($test)) {
+          $value = $test["value"];
+          $operator = $test["operator"];
+          $select->condition($field, $value, $operator);
+        } else $select->condition($field, $test);
+      }
+
+      // Return the result as an object
+      $select->execute()->setFetchMode(PDO::FETCH_CLASS, $className); //prepare class
+      $records = $select->execute()->fetchAll();
+
+      // return to standard Drupal database
+      Database::setActiveConnection();
+      return $records;
+    } //ezacReadAll
+
+    /**
+     * Update an entry in the database.
+     *
+     * @param string $table
+     *   The EZAC table to be used for the database operation
+     *
+     * @return int
+     *   The number of updated rows.
+     *
+     * @see db_update()
+     */
+    public function ezacUpdate($table)
+    {
+        $messenger = Drupal::messenger();
+
+        // EZAC database is outside the Drupal structure
+        Database::setActiveConnection(self::dbName);
+        $db = Database::getConnection();
+
+        // build array from object fields
+        $entry = get_object_vars($this);
+
+        try {
+            // db_update()...->execute() returns the number of rows updated.
+            $update = $db->update($table)
+                ->fields($entry);
+            $update->condition('id', $entry['id']);
+            $count = $update->execute();
+        } catch (Exception $e) {
+            $message = 'db_update failed. Message = ' . $e->getMessage();
+            $messenger->addMessage($message, $messenger::TYPE_ERROR);
+        }
+
+        // return to standard Drupal database
+        Database::setActiveConnection();
+
+        /** @var int $count */
+        return $count;
+
+    }  // ezacUpdate
+
+    /**
+     * Delete an entry from the database.
+     *
+     * @param string $table
+     *   The EZAC table to be used for the database operation
+     * @return int
+     *   records_deleted
+     * @see db_delete()
+     */
+    public function ezacDelete($table)
+    {
+
+        // EZAC database is outside the Drupal structure
+        Database::setActiveConnection(self::dbName);
+        $db = Database::getConnection();
+
+        // prepare delete
+        $select = $db->delete($table);
+        $select->condition('id', $this->id);
+        $records_deleted = $select->execute();
+
+        // return to standard Drupal database
+        Database::setActiveConnection();
+
+        return $records_deleted;
+
+    }  // ezacDelete
+
+} // EzacStorage
---
+++ modules/contrib/EzacStarts/src/Controller/EzacStartsController.php
@@ -0,0 +1,387 @@
+<?php
+
+namespace Drupal\ezacStarts\Controller;
+
+use Drupal;
+use Drupal\Core\Pager\PagerManager;
+use Drupal\Core\Pager\PagerManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
+
+use Drupal\ezacStarts\Model\EzacStart;
+use Drupal\ezac\Util\EzacUtil;
+
+/**
+ * Controller for EZAC start administration.
+ */
+class EzacStartsController extends ControllerBase {
+
+  /**
+   * Display the status of the EZAC starts table
+   * @return array
+   */
+  public function status() {
+    $content = [];
+
+    //$schema = drupal_get_module_schema('Ezac', 'starts');
+
+    // show record count for each Code value
+    $headers = [
+      t("Jaar"),
+      t("Aantal vliegdagen"),
+      t("Uitvoer"),
+    ];
+    
+    $total = 0;
+    $condition = [];
+    $datums = array_unique(EzacStart::index($condition, 'datum', 'datum','DESC'));
+    $jaren = [];
+    foreach ($datums as $datum) {
+      $dp = date_parse($datum);
+      $year = $dp['year'];
+      if (isset($jaren[$year])) $jaren[$year]++;
+      else $jaren[$year] = 1;
+    }
+    foreach ($jaren as $jaar => $aantal) {
+      $count = $aantal;
+      $total = $total+$count;
+      $urlJaar = Url::fromRoute(
+        'ezac_starts_overzicht_jaar',
+        [
+          'jaar' => $jaar
+        ]
+      )->toString();
+      $urlExport = Url::fromRoute(
+        'ezac_starts_export_jaar',
+        [
+          'filename' => "Starts-$jaar.csv",
+          'jaar' => $jaar
+        ]
+      )->toString();
+      $rows[] = [
+        t("<a href=$urlJaar>$jaar</a>"),
+        $count,
+        t("<a href=$urlExport>Starts-$jaar.csv</a>"),
+      ];
+    }
+    // add line for totals
+    $urlExport = Url::fromRoute(
+      'ezac_starts_export',
+      [
+        'filename' => "Starts.csv",
+      ]
+    )->toString();
+    $rows[]= [
+      t('Totaal'),
+      $total,
+      t("<a href=$urlExport>Starts.csv</a>"),
+    ];
+    //build table
+    $content['table'] = [
+      '#type' => 'table',
+      '#caption' => t("Jaar overzicht van het EZAC STARTS bestand"),
+      '#header' => $headers,
+      '#rows' => $rows,
+      '#empty' => t('Geen gegevens beschikbaar.'),
+      '#sticky' => TRUE,
+    ];
+
+    // Don't cache this page.
+    $content['#cache']['max-age'] = 0;
+
+    return $content;
+  }
+
+    /**
+     * Render a list of entries in the database.
+     * @param string
+     *  $jaar - categorie (optional)
+     * @return array
+     */
+  public function overzichtJaar($jaar) {
+    $content = array();
+
+    $rows = [];
+    $headers = [
+        t('datum'),
+        t('aantal starts'),
+    ];
+
+    // select all start dates for selected year
+    $condition = [
+        'datum' => [
+            'value' => ["$jaar-01-01", "$jaar-12-31"],
+            'operator' => 'BETWEEN'
+        ],
+    ];
+    $from = null;
+    $range = null;
+    $field = 'datum';
+    $sortkey = null;
+    $sortdir = null;
+    $unique = TRUE; // return unique results only
+
+    // bepaal aantal dagen
+    $total = count(EzacStart::index($condition, $field, $sortkey, $sortdir, $from, $range, $unique));
+
+    // prepare pager
+    $range = 120;
+    $pager = \Drupal::service('pager.manager')
+      ->createPager($total, $range);
+    $page = $pager
+      ->getCurrentPage();
+
+    $from = $range * $page;
+    $field = 'datum';
+    $sortkey = 'datum';
+    $sortdir = 'ASC';
+
+    $startsDates = EzacStart::index($condition, $field, $sortkey, $sortdir);
+    $startsIndex = array_unique($startsDates);
+
+    $dagen = [];
+    foreach ($startsDates as $datum) {
+      if (isset($dagen[$datum])) $dagen[$datum]++;
+      else $dagen[$datum] = 1;
+    }
+
+    foreach ($startsIndex as $datum) {
+      $urlString = Url::fromRoute(
+        'ezac_starts_overzicht',  // show starts for datum
+        [
+          'datum_start' => $datum,
+          'datum_eind' => $datum,
+        ]
+      )->toString();
+
+      $d = EzacUtil::showDate($datum);
+      $rows[] = [
+        //link each record to overzicht route
+        t("<a href=$urlString>$d"),
+        $dagen[$datum],
+      ];
+    }
+    $caption = "Overzicht EZAC Starts data voor $jaar";
+    $content['table'] = [
+      '#type' => 'table',
+      '#caption' => $caption,
+      '#header' => $headers,
+      '#rows' => $rows,
+      '#empty' => t('Geen gegevens beschikbaar.'),
+      '#sticky' => TRUE,
+    ];
+    // add pager
+    $content['pager'] = [
+      '#type' => 'pager',
+      '#weight' => 5
+    ];
+    // Don't cache this page.
+    $content['#cache']['max-age'] = 0;
+
+    return $content;
+  } // overzichtJaar
+
+  /**
+   * @param $datum_start
+   * @param $datum_eind
+   * @param null $vlieger
+   * @return array $content
+   */
+  public static function startOverzicht($datum_start, $datum_eind, $vlieger = NULL) {
+    //@TODO this routine to be removed here
+    $content = array();
+
+    $rows = [];
+    $headers = [
+      t('datum'),
+      t('start'),
+      t('landing'),
+      t('duur'),
+      t('registratie'),
+      t('gezagvoerder'),
+      t('tweede'),
+      t('soort'),
+      t('start methode'),
+      t('instructie'),
+      t('opmerking'),
+    ];
+
+    $leden = EzacUtil::getLeden();
+    // $kisten = EzacUtil::getKisten();
+
+    // select all starts for selected date
+    if (isset($datum_eind))
+    {
+      $condition['datum'] =
+        [
+          'value' => [$datum_start, $datum_eind],
+          'operator' => 'BETWEEN',
+        ];
+    }
+    else $condition = ['datum' => $datum_start];
+
+    if (isset($vlieger)) {
+      // add orGroup to selection
+      $condition['OR'] =
+        [
+          'gezagvoerder' => $vlieger,
+          'tweede' => $vlieger,
+        ];
+    }
+
+    // prepare pager
+    $total = EzacStart::counter($condition);
+    $field = 'id';
+    $sortkey = 'start';
+    $sortdir = 'ASC'; // newest first
+    $limit = 100;
+    //$page = pager_default_initialize($total, $range); // deprecated
+    $pager = \Drupal::service('pager.manager')
+      ->createPager($total, $limit);
+    $page = $pager
+      ->getCurrentPage();
+
+    $from = $limit * $page;
+    $unique = FALSE; // return all results
+
+    $startsIndex = EzacStart::index($condition, $field, $sortkey, $sortdir, $from, $limit, $unique);
+    foreach ($startsIndex as $id) {
+      $start = (new EzacStart)->read($id);
+
+      $urlString = Url::fromRoute(
+        'ezac_starts_update',  // edit starts record
+        ['id' => $start->id]
+      )->toString();
+
+      if (isset($leden[$start->gezagvoerder]) && $start->gezagvoerder <> '') {
+        $gezagvoerder = $leden[$start->gezagvoerder];
+      }
+      else $gezagvoerder = $start->gezagvoerder; // un-edited record value
+
+      if (isset($leden[$start->tweede]) && $start->tweede <> '') {
+        $tweede = $leden[$start->tweede];
+      }
+      else $tweede = $start->tweede; // un-edited record value
+
+      $rows[] = [
+        //link each record to edit route
+        $start->datum,
+        t("<a href=$urlString>" .substr($start->start, 0, 5) ."</a>"),
+        substr($start->landing,0,5),
+        substr($start->duur, 0,5),
+        $start->registratie,
+        $gezagvoerder,
+        $tweede,
+        EzacStart::$startSoort[$start->soort],
+        EzacStart::$startMethode[$start->startmethode],
+        ($start->instructie) ? 'Ja' :'',
+        $start->opmerking,
+      ];
+    }
+    $d = EzacUtil::showDate($datum_start);
+    if ($datum_eind <> $datum_start) $d.= " tot " .EzacUtil::showDate($datum_eind);
+    $caption = "Overzicht EZAC Starts bestand $d";
+    $content['table'] = [
+      '#type' => 'table',
+      '#caption' => $caption,
+      '#header' => $headers,
+      '#rows' => $rows,
+      '#empty' => t('Geen gegevens beschikbaar.'),
+      '#sticky' => TRUE,
+    ];
+    // add pager
+    $content['pager'] = [
+      '#type' => 'pager',
+      '#weight' => 5
+    ];
+
+    return $content;
+  }
+    /**
+     * Render a list of entries in the database.
+     * @param string $datum_start
+     * @param string $datum_eind
+     * @return array
+     */
+    public function overzicht($datum_start = NULL, $datum_eind = NULL) {
+      //@todo add filter params for vlieger and registratie
+      $content = self::startOverzicht($datum_start, $datum_eind, NULL);
+      // Don't cache this page.
+      $content['#cache']['max-age'] = 0;
+
+      return $content;
+    } // overzicht
+
+    /**
+     * Maak exportbestand uit Starts tabel
+     * geformatteerd voor input in bestand (csv)
+     * Output via html headers naar attachment
+     *
+     * @param string $filename
+     * @param null $jaar
+     * @return mixed Response output text in csv format
+     *   output text in csv format
+     */
+  public function export($filename = 'Ezac.txt', $jaar = NULL) {
+
+    $messenger = Drupal::messenger();
+
+    if ($filename == '') $filename = 'Ezac.txt';
+
+    // Determine Jaar  from Starts for export
+    if (isset($jaar)) {
+        $condition = [
+            'datum' => [
+                'value' => ["$jaar-01-01", "$jaar-12-31"],
+                'operator' => 'BETWEEN'
+            ],
+        ];
+    }
+    else $condition = []; //select all active records
+
+    $records = EzacStart::index($condition); //read records index
+    $count = count($records);
+    $messenger->addMessage("Export $count records voor jaar [$jaar] naar bestand [$filename]"); //DEBUG
+
+    $output = ""; // initialize output
+    //build header line
+    foreach (EzacStart::$fields as $field => $description) {
+      $output .= '"' .$field .'";';
+    }
+    //remove last ";" 
+    $output = rtrim($output, ";") ."\r\n";
+    
+    // export all records
+    foreach ($records as $id) {
+      $start = (new EzacStart)->read($id);
+      // add all fields
+      foreach (EzacStart::$fields as $field => $description) {
+        $output .= sprintf('"%s";',$start->$field);
+      }
+      //remove last ";" 
+      $output = rtrim($output, ";") ."\r\n";
+    }
+
+    $response = new Response(
+      $output,
+      Response::HTTP_OK,
+      array(
+        'content-type' => 'text/plain',
+      )
+    );
+
+    $disposition = $response->headers->makeDisposition(
+      ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+      $filename
+    );
+    $response->headers->set('Content-Disposition', $disposition);
+    $response->setCharset('UTF-8');
+
+      /** @var mixed $response */
+      return $response;
+  } // export  
+  
+} //class EzacStartsController
