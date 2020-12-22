5ffbeaee5c1266292c76359812e646e01f30ebeb
Evert Fekkes <evert@efekkes.nl>
2020-12-22 16:20:22 +0100

    EzacStartsResource.php post handler added

--- modules/contrib/ezac_starts/src/Plugin/rest/resource/EzacStartsResource.php
+++ modules/contrib/ezac_starts/src/Plugin/rest/resource/EzacStartsResource.php
@@ -4,8 +4,11 @@
 namespace Drupal\ezac_starts\Plugin\rest\resource;
 
 use Drupal;
+use Drupal\rest\ModifiedResourceResponse;
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Drupal\ezac_starts\Model\EzacStart;
@@ -22,6 +25,23 @@ use Drupal\ezac_starts\Model\EzacStart;
    * )
    */
 class EzacStartsResource extends ResourceBase {
+
+  /**
+   * A current user instance.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
+    $instance->logger = $container->get('logger.factory')->get('ezac_starts');
+    $instance->currentUser = $container->get('current_user');
+    return $instance;
+  }
 
   /**
    * @param $datum
@@ -81,7 +101,7 @@ class EzacStartsResource extends ResourceBase {
    *   The response containing the leden record or array of records.
    *
    */
-  public function get() {
+  public function get(): ResourceResponse {
 
     // Configure caching settings.
     $build = [
@@ -180,5 +200,170 @@ class EzacStartsResource extends ResourceBase {
       throw new BadRequestHttpException('No valid parameter provided');
     }
   } //get
+
+  /**
+   * Responds to POST requests.
+   *
+   * @param $datum
+   * @param $registratie
+   * @param $gezagvoerder
+   * @param $tweede
+   * @param $soort
+   * @param $startmethode
+   * @param $start
+   * @param $landing
+   * @param $duur
+   * @param $instructie
+   * @param $opmerking
+   *
+   * @return \Drupal\rest\ModifiedResourceResponse
+   *   The HTTP response object.
+   *
+   */
+  public function post(): ModifiedResourceResponse {
+
+    // Use current user after pass authentication to validate access.
+    /*
+    if (!$this->currentUser->hasPermission('access content')) {
+      throw new AccessDeniedHttpException();
+    }
+    */
+
+    //get parameters
+    $datum = Drupal::request()->query->get('datum');
+    $registratie = Drupal::request()->query->get('registratie');
+    $gezagvoerder = Drupal::request()->query->get('gezagvoerder');
+    $tweede = Drupal::request()->query->get('tweede');
+    $soort = Drupal::request()->query->get('soort');
+    $startmethode = Drupal::request()->query->get('startmethode');
+    $start = Drupal::request()->query->get('start');
+    $landing = Drupal::request()->query->get('landing');
+    $duur = Drupal::request()->query->get('duur');
+    $instructie = Drupal::request()->query->get('instructie');
+    $opmerking = Drupal::request()->query->get('opmerking');
+
+    // Build start record
+    $start_record = new EzacStart();
+
+    // datum
+    if (isset($datum)) {
+      // check datum
+      $dc = explode('-', $datum);
+      if (!checkdate($dc[1], $dc[2], $dc[0])) {
+        throw new BadRequestHttpException("Invalid datum provided: $datum");
+      }
+      $start_record->datum = $datum;
+    }
+    else {
+      throw new BadRequestHttpException('No datum parameter provided');
+    }
+
+    // registratie
+    if (isset($registratie)) {
+      //check registratie to be valid format xx-xxxx
+      if (!strpos($registratie, '-')) {
+        throw new BadRequestHttpException("Invalid registratie: $registratie");
+      }
+      $start_record->registratie = $registratie;
+    }
+    else {
+      throw new BadRequestHttpException('No registratie parameter provided');
+    }
+
+    // gezagvoerder
+    if (isset($gezagvoerder)) {
+      $start_record->gezagvoerder = substr($gezagvoerder, 0, 20);
+    }
+    else {
+      throw new BadRequestHttpException('No gezagvoerder parameter provided');
+    }
+
+    // tweede
+    if (isset($tweede)) {
+      $start_record->tweede = substr($tweede, 0, 20);
+    }
+    // soort
+    if (isset($soort) && $soort != '') {
+      if (!array_key_exists($soort, EzacStart::$startSoort)) {
+        throw new BadRequestHttpException("Invalid soort: $soort");
+      }
+      $start_record->soort = $soort;
+    }
+
+    // startmethode
+    if (isset($startmethode)) {
+      if (!array_key_exists($startmethode, EzacStart::$startMethode)) {
+        throw new BadRequestHttpException("Invalid startmethode: $startmethode");
+      }
+      $start_record->startmethode = $startmethode;
+    }
+    else {
+      throw new BadRequestHttpException('No startmethode parameter provided');
+    }
+
+    // start
+    if (isset($start)) {
+      // check valid time
+      if (!in_array(strlen($start), array(5, 8))) { //HH:MM[:SS]
+        throw new BadRequestHttpException("Invalid start time: $start");
+      }
+      $start_delen = explode(':',$start);
+      if (intval($start_delen[0] > 23)
+        or (intval($start_delen[1] > 59))) {
+        throw new BadRequestHttpException("Invalid start time: $start");
+      }
+      $start = strtotime("$datum $start");
+      $start_record->start = date('Y-m-d H:i:s',$start); //construct valid datetime format
+    }
+    else {
+      // no start provided
+      $start_record->start = date('Y-m-d H:i:s', 0);
+    }
+
+    // landing
+    if (isset($landing)) {
+      // check valid time
+      if (!in_array(strlen($landing), array(5, 8))) { //HH:MM[:SS]
+        throw new BadRequestHttpException("Invalid landing time: $landing");
+      }
+      $landing_delen = explode(':',$landing);
+      if (intval($landing_delen[0] > 23)
+        or (intval($landing_delen[1] > 59))) {
+        throw new BadRequestHttpException("Invalid landing time: $landing");
+      }
+      $landing = strtotime("$datum $landing");
+      if ($landing < $start) {
+        throw new BadRequestHttpException("Landing before start $landing");
+      }
+      $start_record->landing = date('Y-m-d H:i:s',$landing); //construct valid datetime format
+      //duur is calculated
+      $start_record->duur = date('Y-m-d H:i:s', $landing-$start);
+    }
+    else {
+      // no landing provided
+      $start_record->landing = date('Y-m-d H:i:s', 0);
+      $start_record->duur = date('Y-m-d H:i:s', 0);
+    }
+
+    // duur is calculated from start-landing
+
+    // instructie
+    if (isset($instructie)) {
+      if (!in_array(intval($instructie), [0, 1])) {
+        throw new BadRequestHttpException("Invalid instructie: $instructie");
+      }
+      $start_record->instructie = intval($instructie);
+    }
+
+    // opmerking
+    //opmerking is optional, max 30 chars
+    if (isset($opmerking)) {
+      $start_record->opmerking = substr(trim($start_record->opmerking), 0, 30);
+    }
+
+    // write start record to database
+    $id = $start_record->create();
+    return new ModifiedResourceResponse($id, 200);
+  } // post
 
 }
