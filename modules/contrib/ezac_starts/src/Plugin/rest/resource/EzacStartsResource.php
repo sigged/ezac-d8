01c74b118c55d955496553db202b56a3234a6fce
Evert Fekkes <evert@efekkes.nl>
2020-12-23 17:26:10 +0100

    EzacStartsResource.php nrAffected in delete

--- modules/contrib/ezac_starts/src/Plugin/rest/resource/EzacStartsResource.php
+++ modules/contrib/ezac_starts/src/Plugin/rest/resource/EzacStartsResource.php
@@ -459,10 +459,9 @@ class EzacStartsResource extends ResourceBase {
       throw new BadRequestHttpException("Invalid datum: $datum");
     }
     // delete record
+    $record = new EzacStart;
+    $record->id = $id;
     $nrAffected = $record->delete();
-    if ($nrAffected == NULL) {
-      $nrAffected = 0;
-    }
     return new ModifiedResourceResponse($nrAffected, 200);
   } //delete
 
